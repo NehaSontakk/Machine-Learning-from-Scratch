09630af9708fa927c37edb2c9cb2b57f66fc51d8
NehaSontakk <nsontakke004@gmail.com>
2019-04-27 21:58:05 +0530

    Create kmeans.py

---
+++ K-Means/kmeans.py
@@ -0,0 +1,74 @@
+import numpy as np
+import matplotlib.pyplot as plt
+points = np.array([[0.1,0.6],[0.15,0.71],[0.08,0.9],[0.16,0.85],[0.2,0.3],[0.25,0.5],[0.24,0.1],[0.3,0.2]])
+plt.scatter(points[:,0],points[:,1],s=100,color='red')
+plt.show()
+
+
+class K_Means:
+    def __init__(self, k=2, tol=0.001, max_iter=300):
+        self.k = k
+        self.tol = tol
+        self.max_iter = max_iter
+
+    def fit(self,data):
+
+        self.centroids = {}
+
+        for i in range(self.k):
+            self.centroids[i] = data[i]
+	    print self.centroids[i]
+
+        for i in range(self.max_iter):
+            self.classifications = {}
+
+            for i in range(self.k):
+                self.classifications[i] = []
+
+            for featureset in data:
+                distances = [np.linalg.norm(featureset-self.centroids[centroid]) for centroid in self.centroids]
+		print "dist",distances
+                classification = distances.index(min(distances))
+		print "class",classification
+                self.classifications[classification].append(featureset)
+
+            prev_centroids = dict(self.centroids)
+
+            for classification in self.classifications:
+                self.centroids[classification] = np.average(self.classifications[classification],axis=0)
+		print self.centroids[classification]
+                
+
+            optimized = True
+
+            for c in self.centroids:
+                original_centroid = prev_centroids[c]
+                current_centroid = self.centroids[c]
+                if np.sum((current_centroid-original_centroid)/original_centroid*100.0) > self.tol:
+                    print("umm",np.sum((current_centroid-original_centroid)/original_centroid*100.0))
+                    optimized = False
+
+            if optimized:
+                break
+
+    def predict(self,data):
+        distances = [np.linalg.norm(data-self.centroids[centroid]) for centroid in self.centroids]
+        classification = distances.index(min(distances))
+        return classification
+		
+		
+clf = K_Means()
+clf.fit(points)
+colors = 10*["g","r","c","b","k"]		
+for centroid in clf.centroids:
+	plt.scatter(clf.centroids[centroid][0], clf.centroids[centroid][1],
+                marker="o", color="k", s=150, linewidths=5)
+
+for classification in clf.classifications:
+    color = colors[classification]
+    for featureset in clf.classifications[classification]:
+        plt.scatter(featureset[0], featureset[1], marker="x", color=color)
+        
+plt.show()	
+			
+			
